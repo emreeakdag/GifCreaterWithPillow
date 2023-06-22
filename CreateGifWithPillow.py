b04a8fc9f64cf145b00a58904698f2dd0fd897d9
Emre Akdağ <emreakdagoffical@gmail.com>
2023-06-22 13:22:30 +0300

    Created second list for images

--- CreateGifWithPillow.py
+++ CreateGifWithPillow.py
@@ -1,6 +1,8 @@
 from PIL import Image
 
 
-# Fotoğraflar için liste oluşturalım.
+# Fotoğrafların yolu için liste oluşturalım.
 image_path_list = ['dog-1.jpg', 'dog-2.jpg', 'dog-3.jpg', 'dog-4.jpg']
 
+# Resimler için bir liste oluşturalım
+image_list = [Image.open(file) for file in image_path_list]
